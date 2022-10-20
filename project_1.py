d4f69d59e8b46e6ff68f1ade169e7ace053c8c3a
Saravana kumar <66402906+saravanasathie@users.noreply.github.com>
2022-10-20 17:09:13 +0530

    Add files via upload

---
+++ project_1.py
@@ -0,0 +1,14 @@
+'''
+Output:
+Welcome to the Band Name Generator.
+What's name of the city you grew up in?
+Madurai         =>  Input
+What's your pet's name?
+Dog             =>  Input
+Your band name could be Madurai Dog
+'''
+
+print("Welcome to the Band Name Generator.")
+a = input("What's name of the city you grew up in?\n")
+b = input("What's your pet's name\n")
+print("Your band name could be "+a+" "+b)
