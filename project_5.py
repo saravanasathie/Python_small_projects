e78c5b1cf3db827e8c875d0557535940101587db
Saravana kumar <66402906+saravanasathie@users.noreply.github.com>
2022-10-21 23:35:59 +0530

    Add files via upload

---
+++ project_5.py
@@ -0,0 +1,29 @@
+"""
+Output:
+size = "L"          => Input
+add_pepperoni = "Y"     => Input
+extra_cheese = "N"      => Input
+Your final bill is: $28.
+"""
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
